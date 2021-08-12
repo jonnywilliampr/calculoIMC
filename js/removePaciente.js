9c41f377939310e33236a6f21f0e75551315c222
Jonny William <jonnywilliampr@gmail.com>
2021-08-12 20:53:14 -0300

    feat: adicionando um filtro com expressão regular.

--- js/removePaciente.js
+++ js/removePaciente.js
@@ -1,5 +1,9 @@
 var tabela = document.querySelectorAll("table"); //pega a tudo da tabela
 
 tabela = addEventListener("dblclick", (event)=>{ //escutando o evento a partir de um filho.
-    event.target.parentNode.remove(); //parentNode permite que seja selecionado o Pai/tr do Filho/td clicado.
+    event.target.parentNode.classList.add("fadeOut");
+
+    setTimeout(()=>{
+        event.target.parentNode.remove(); //parentNode permite que seja selecionado o Pai/tr do Filho/td clicado.
+    },500);
 });
